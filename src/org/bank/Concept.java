c02a3828314f1a1c660fb09490bd15b80be822f5
HemalathaVell <hemavel4440@gmail.com>
2024-10-06 22:13:17 +0530

    firstname

---
+++ src/org/bank/Concept.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Concept {
+public static void main(String[] args) {
+	WebDriver driver = new EdgeDriver();
+	driver.get("https://citizensbank.com/homepage.aspx");
+	driver.findElement(By.Xpath)
+	
+	
+}
+	
+
+}
